fd12fcfb33f397c763cef34d2eeb2af21c0658cc
Chris Hamm <chamm@ku.edu>
2016-06-24 15:43:04 -0600

    moved .R files to R/ directory

--- R/Count_species.R
+++ R/Count_species.R
@@ -34,42 +34,39 @@ str(SJER_slimmed)
 head(SJER_slimmed)
 
 
-# compute K means
+### compute K means
 K_hsi <- kmeans(SJER_slimmed, centers = 10, iter.max = 10)
 str(K_hsi)
 hist(K_hsi$cluster)
 
-# alternatively PCA
+
+### PCA 
 SJER_PCA1 <- prcomp(SJER_slimmed, center = TRUE) 
 str(SJER_PCA1)
 plot(SJER_PCA1, ylim = c(0, 1))
 summary(SJER_PCA1)$importance
 
 
-
-SJER_PCA2 <- princomp(SJER_matrix) # big difference between "princomp" and "prcomp" with centering
-
-str(SJER_PCA2)
-plot(SJER_PCA2, ylim = c(0, 1))
-loadings(SJER_PCA2)
+# SJER_PCA2 <- princomp(SJER_matrix) # big difference between "princomp" and "prcomp" with centering
+# str(SJER_PCA2)
+# plot(SJER_PCA2, ylim = c(0, 1))
+# loadings(SJER_PCA2)
 
 # extract PCs 1:3
-pc1 <- SJER_PCA1$x[, 1:3]
-str(pc1)
-plot3d(pc1[, 1], pc1[, 2], pc1[, 3])
-
-pca_km <- kmeans(pc1, centers = 3)
+# pc1 <- SJER_PCA1$x[, 1:3]
+# str(pc1)
+# plot3d(pc1[, 1], pc1[, 2], pc1[, 3])
+# pca_km <- kmeans(pc1, centers = 3)
 
 # using mclust to generate K estimates
-# SJER_mc1 <- Mclust(SJER_matrix, G = 1:3, warn = TRUE)
+SJER_mc1 <- Mclust(SJER_slimmed, G = 1:3, warn = TRUE)
+plot(SJER_mc1)
+
+
 
 SJ_bc1 <- mclustBIC(SJER_slimmed, G = 1:25, modelNames = c("EII", "VII", "EEI", "VEI", "EEE"))
 summary(SJ_bc1)
 plot(SJ_bc1)
 
 
-plot(mclust_test)
-
-summary(mclust_test)
-
-#xyFromCell in raster - to retain 
+# use xyFromCell in raster - to retain 
